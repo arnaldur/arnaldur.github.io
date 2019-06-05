fc07a8f6f3fb3af1e84afdcdb469e9191afd1e51
Arnaldur Hilmisson <arnaldur@gmail.com>
2019-06-05 13:33:40 +0200

    Updates

---
+++ precache-manifest.7861e8758762f21d08de645b5e054169.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c04f2c0e3b168cb510d86a187a1d913b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cecf23317e2978caeac7",
+    "url": "/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "282cbe3165e2889fd2d3",
+    "url": "/static/js/2.e3bf2b39.chunk.js"
+  },
+  {
+    "revision": "cecf23317e2978caeac7",
+    "url": "/static/js/main.a5650ea3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7098ef04e278ff7b58368091d6e36df8",
+    "url": "/static/media/logo.7098ef04.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/uppskriftir2/precache-manifest.9c8ea0f7fd9e940d89b1f9edbfe278f3.js"
+  "/precache-manifest.7861e8758762f21d08de645b5e054169.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/uppskriftir2/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
