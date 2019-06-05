22ed3277eff726058b53cc4bdb52c4489c4e7598
Arnaldur Hilmisson <arnaldur@gmail.com>
2019-06-05 12:37:13 +0200

    Updates

---
+++ precache-manifest.09553ab78309ba1ec6be2a68373d1bf0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbdb38af73f47cf548f86027d328f37e",
+    "url": "/uppskriftir2/index.html"
+  },
+  {
+    "revision": "1fa1762022899aeae46e",
+    "url": "/uppskriftir2/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "282cbe3165e2889fd2d3",
+    "url": "/uppskriftir2/static/js/2.e3bf2b39.chunk.js"
+  },
+  {
+    "revision": "1fa1762022899aeae46e",
+    "url": "/uppskriftir2/static/js/main.fa2668c1.chunk.js"
+  },
+  {
+    "revision": "212f61127ea55bccb23f",
+    "url": "/uppskriftir2/static/js/runtime~main.16e0aab8.js"
+  },
+  {
+    "revision": "7098ef04e278ff7b58368091d6e36df8",
+    "url": "/uppskriftir2/static/media/logo.7098ef04.svg"
+  }
+]);
